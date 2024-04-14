692af9fa9e26f2388ba4165d2a27e2a7322d3d24
Ester Martínez <ester.frontend@gmail.com>
2024-04-14 14:36:29 +0200

    Fix: crea componente no utilizado para crear clases en tailwind

--- app/ui/components/Backgrounds/Backgrounds.jsx
+++ app/ui/components/Backgrounds/Backgrounds.jsx
@@ -3,6 +3,9 @@ export default function Backgrounds () {
         <>
             <div className="bg-tailor-blue"></div>
             <div className="bg-tailor-gray"></div>
+            <div className="fill-tailor-blue"></div>
+            <div className="fill-gray-300"></div>
+            <div className="fill-tailor-purple"></div>
         </>
     )
 }
