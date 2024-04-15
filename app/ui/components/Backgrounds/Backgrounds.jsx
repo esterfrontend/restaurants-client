46852e50524a4f1b31b1c8f9702a49b17c5d8b5d
Ester Martínez <ester.frontend@gmail.com>
2024-04-15 18:53:27 +0200

    Fix: Cambio ruta imagen home y añado bg de imágenes para tailwind

--- app/ui/components/Backgrounds/Backgrounds.jsx
+++ app/ui/components/Backgrounds/Backgrounds.jsx
@@ -6,6 +6,9 @@ export default function Backgrounds () {
             <div className="fill-tailor-blue"></div>
             <div className="fill-gray-300"></div>
             <div className="fill-tailor-purple"></div>
+            <div className="bg-home"></div>
+            <div className="bg-signup"></div>
+            <div className="bg-login"></div>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         'tailor-purple': '#8DA0F0'
       },
       backgroundImage: {
-        'home': "url('https://res.cloudinary.com/dsywb80za/image/upload/v1712954027/Restaurants/Home_ztnfpj.jpgg')",
+        'home': "url('https://res.cloudinary.com/dsywb80za/image/upload/v1712954027/Restaurants/Home_ztnfpj.jpg')",
         'login': "url('https://res.cloudinary.com/dsywb80za/image/upload/v1712954201/Restaurants/Picture_shzy4m.jpg')",
         'signup': "url('https://res.cloudinary.com/dsywb80za/image/upload/v1712954027/Restaurants/Signin_wk08ia.jpg')",
       }
