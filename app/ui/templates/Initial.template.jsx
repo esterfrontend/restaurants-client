dccaa03054d1db5635b10f3dd8e190de430c3d6c
Ester Martínez <ester.frontend@gmail.com>
2024-04-16 19:23:08 +0200

    Logo en mayúscula

--- app/ui/templates/Initial.template.jsx
+++ app/ui/templates/Initial.template.jsx
@@ -1,7 +1,10 @@
+import Logo from "../components/Logo/Logo";
+
 export default function InitialTemplate({children, page, bgColor='tailor-blue', logoColor}) {
     return (
         <div className="flex flex-col lg:flex-row gap-5 lg:gap-10 items-end p-5 lg:p-10 flex-1">
             <div className={`md:w-1/2 flex flex-col gap-5 lg:gap-10 bg-${bgColor} rounded-xl p-6`}>
+                <Logo fill={logoColor} className='w-[100px] lg:w-[190px]'/>
                 {children}
             </div>
             <div className={`w-full md:w-1/2 h-[300px] md:h-full overflow-hidden rounded-xl relative bg-${page} bg-cover bg-center`}></div>
